2958bfac0bd39932f2a2fd466e947b4debcf0b74
grkmyvz <gorkem.yavuz.39@hotmail.com>
2023-10-29 03:22:31 +0300

    Navbar alignment

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -25,41 +25,44 @@ export default function Home() {
       <main className={inter.className}>
         <div className="w100 horizontal-stack">
           <h1>Scroffiti</h1>
-          <w3m-button />
-          <a
-            href="https://github.com/grkmyvz/scroffiti"
-            aria-label="GitHub"
-            title="GitHub"
-            style={{ marginRight: '1rem' }}
-            target="_blank"
-          >
-            <svg
-              aria-hidden="true"
-              width="32"
-              height="32"
-              version="1.1"
-              viewBox="0 0 16 16"
-              fill="#fff"
+          <div className="horizontal-stack">
+            <w3m-button />
+            <a
+              href="https://github.com/grkmyvz/scroffiti"
+              aria-label="GitHub"
+              title="GitHub"
+              style={{ marginRight: '1rem', marginLeft: '2rem' }}
+              target="_blank"
             >
-              <path
-                fill-rule="evenodd"
-                d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"
-              ></path>
-            </svg>
-          </a>
+              <svg
+                aria-hidden="true"
+                width="32"
+                height="32"
+                version="1.1"
+                viewBox="0 0 16 16"
+                fill="#fff"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"
+                ></path>
+              </svg>
+            </a>
+          </div>
         </div>
         <hr />
-        {selectedNetworkId !== 534352 && (
-          <>
-            <div className="w100 bg-warning p1 text-center">
-              <p>
-                <strong>Warning: </strong> Please select the <u>Scroll</u> network, which is a valid
-                network.
-              </p>
-            </div>
-            <hr />
-          </>
-        )}
+        {selectedNetworkId !== 534352 ||
+          (selectedNetworkId !== undefined && (
+            <>
+              <div className="w100 bg-warning p1 text-center">
+                <p>
+                  <strong>Warning: </strong> Please select the <u>Scroll</u> network, which is a
+                  valid network.
+                </p>
+              </div>
+              <hr />
+            </>
+          ))}
         <BestScroffiti showToast={showToast} />
         <hr />
         <SendScroffiti showToast={showToast} />
